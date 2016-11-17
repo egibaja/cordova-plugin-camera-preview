b1ee9f522b0bee15b38801f8cb9bd45e8060c02a
Christopher Manouvrier <cmanou@mac.com>
2016-11-17 11:14:53 +1100

    Set optimal size to highest resolution and require camera

--- src/android/com/mbppower/CameraActivity.java
+++ src/android/com/mbppower/CameraActivity.java
@@ -694,38 +694,11 @@ class Preview extends RelativeLayout implements SurfaceHolder.Callback {
         }
     }
     private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
-        final double ASPECT_TOLERANCE = 0.1;
-        double targetRatio = (double) w / h;
-        if (displayOrientation == 90 || displayOrientation == 270) {
-            targetRatio = (double) h / w;
-        }
+        //For Now We get the highest res image.
+        //TODO: Change to allow user specified quality, and allow checking of sizes
         if (sizes == null) return null;
-
-        Camera.Size optimalSize = null;
-        double minDiff = Double.MAX_VALUE;
-
-        int targetHeight = h;
-
-        // Try to find an size match aspect ratio and size
-        for (Camera.Size size : sizes) {
-            double ratio = (double) size.width / size.height;
-            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
-            if (Math.abs(size.height - targetHeight) < minDiff) {
-                optimalSize = size;
-                minDiff = Math.abs(size.height - targetHeight);
-            }
-        }
-
-        // Cannot find the one match the aspect ratio, ignore the requirement
-        if (optimalSize == null) {
-            minDiff = Double.MAX_VALUE;
-            for (Camera.Size size : sizes) {
-                if (Math.abs(size.height - targetHeight) < minDiff) {
-                    optimalSize = size;
-                    minDiff = Math.abs(size.height - targetHeight);
-                }
-            }
-        }
+        
+        Camera.Size optimalSize = sizes.get(0);
 
         Log.d(TAG, "optimal preview size: w: " + optimalSize.width + " h: " + optimalSize.height);
         return optimalSize;
